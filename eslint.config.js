dce1b8ff1e825de4793055f2e7909140a85cea21
nikitaaverochkin <dota.nikita@inbox.ru>
2025-05-25 10:41:05 +0300

    Revert "edit TODO msg"
    This reverts commit 6d595001a37645a4124b8260cec2f93ceabc9d17.

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from "eslint/config";
 import solid from "eslint-plugin-solid";
 import tsParser from "@typescript-eslint/parser";
 
-/**
- * TODO:
- * 1. Вначале - правильно линтить ts - чтобы все было строго и тд
- * 2. Линтить solid (контролировать чтобы пропсы не деструктурировались,
- * чтобы реактивность сохранялась и тд)
- * 3. Линтить codestyle - import order, отступы и тд
- */
+// TODO:
+// 1. Вначале - правильно линтить ts - чтобы все было строго и тд
+// 2. Линтить solid (контролировать чтобы пропсы не деструктурировались,
+// чтобы реактивность сохранялась и тд)
+// 3. Линтить codestyle - import order, отступы и тд
 
 export default defineConfig([
     {
