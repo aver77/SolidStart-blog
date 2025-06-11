4458e9c64a9b6a4cf48b53cbf8390a91a65c840a
nikitaaverochkin <dota.nikita@inbox.ru>
2025-06-11 20:05:33 +0300

    found and remove last css file - dotGrid.module.css

--- src/modules/mainModule/sections/about/dotsGrid.tsx
+++ src/modules/mainModule/sections/about/dotsGrid.tsx
@@ -1,21 +1,33 @@
 import { For } from "solid-js";
 
-import cx from "classnames";
-
-import classes from "./dotsGrid.module.css";
-
 const dotsData = Array(500).fill(undefined);
 
 const Dots = () => {
-    const mediaClass = "ipadLg:!right-offset8x ipadSm:!left-offset3x";
-
     return (
-        <div class={cx(classes["dots"], mediaClass)}>
+        <div class={`
+          top-offset3x bottom-offset3x absolute right-[96px] z-1 grid
+          grid-cols-[repeat(25,1fr)] overflow-hidden
+          ipadLg:!right-offset8x
+          ipadSm:!left-offset3x
+        `}>
             <For each={dotsData}>
                 {(_) => {
                     return (
-                        <div class={classes["dot-wrapper"]}>
-                            <div class={classes["dot"]} />
+                        <div class={`
+                          p-offset2x group h-fit cursor-crosshair rounded-md
+                          transition duration-300
+                          hfa:bg-gray
+                          light:hfa:bg-warmDWhite
+                        `}>
+                            <div class={`
+                              h-offset2x w-offset2x from-dark rounded-full
+                              bg-linear-to-b to-white opacity-35
+                              light:opacity-100 light:from-warmDWhite
+                              light:to-warmWhite light:group-hfa:from-warmDWhite
+                              light:group-hfa:to-gold
+                              group-hfa:from-dark group-hfa:to-gold
+                              group-hfa:opacity-100
+                            `} />
                         </div>
                     );
                 }}
