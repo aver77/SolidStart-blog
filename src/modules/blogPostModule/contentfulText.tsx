df35b9cc6c5e298971c003490b5bdf1e5d7257aa
nikitaaverochkin <dota.nikita@inbox.ru>
2025-06-17 23:44:27 +0300

    Revert "edited contentfulText.tsx"
    This reverts commit 40a78205ad18f1bca4275a2a8607f1e20d63be55.

--- src/modules/blogPostModule/contentfulText.tsx
+++ src/modules/blogPostModule/contentfulText.tsx
@@ -9,29 +9,15 @@ interface IContentfulText {
 }
 
 const ContentfulText: Component<IContentfulText> = (props) => {
-    const getContentfulText = () => {
-        if (!props.text) {
-            return null;
-        }
-
-        const wrapper = document.createElement("div");
-        wrapper.className = cx("contentful-text", `
-          gap-offset8x flex flex-col
-          phones:gap-offset6x
-        `);
-        wrapper.innerHTML = documentToHtmlString(props.text);
-
-        const links = wrapper.querySelectorAll("a");
-
-        links.forEach((link) => {
-            link.setAttribute("target", "_blank");
-            link.setAttribute("rel", "noreferrer noopener");
-        });
-
-        return wrapper;
-    };
-
-    return <>{getContentfulText()}</>;
+    return (
+        <div
+            class={cx("contentful-text", `
+              gap-offset8x flex flex-col
+              phones:gap-offset6x
+            `)}
+            innerHTML={documentToHtmlString(props.text)}
+        />
+    );
 };
 
 export default ContentfulText;
