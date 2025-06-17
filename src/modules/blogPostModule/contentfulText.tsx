3d7ac5eda4e335490e84039afa860d0f2aab2b3d
nikitaaverochkin <dota.nikita@inbox.ru>
2025-06-17 23:46:42 +0300

    fixed document bug in contentfulText.tsx

--- src/modules/blogPostModule/contentfulText.tsx
+++ src/modules/blogPostModule/contentfulText.tsx
@@ -3,14 +3,29 @@ import type { Component } from "solid-js";
 import cx from "classnames";
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 import type { Document } from "@contentful/rich-text-types";
+import {onMount} from "solid-js";
 
 interface IContentfulText {
     text: Document;
 }
 
 const ContentfulText: Component<IContentfulText> = (props) => {
+    let ref: HTMLDivElement | undefined;
+
+    onMount(() => {
+        if (ref) {
+            const links = ref.querySelectorAll("a");
+
+            links.forEach((link) => {
+                link.setAttribute("target", "_blank");
+                link.setAttribute("rel", "noreferrer noopener");
+            });
+        }
+    })
+
     return (
         <div
+            ref={ref}
             class={cx("contentful-text", `
               gap-offset8x flex flex-col
               phones:gap-offset6x
